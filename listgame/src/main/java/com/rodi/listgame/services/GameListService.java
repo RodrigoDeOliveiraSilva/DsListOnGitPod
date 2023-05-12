e8714eab8de93c0661744273118863e40ef2f5e1
RodgoDeOliveiraSilva <rodrigodeoliveirasilva20@gmail.com>
2023-05-11 23:23:59 -0300

    Implements Transactional readOnly = true.

--- listgame/src/main/java/com/rodi/listgame/services/GameListService.java
+++ listgame/src/main/java/com/rodi/listgame/services/GameListService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.rodi.listgame.dto.GameListDTO;
 import com.rodi.listgame.repositories.GameListRepository;
@@ -14,6 +15,7 @@ public class GameListService {
     @Autowired
     GameListRepository gameListRepository;
 
+    @Transactional(readOnly = true)
     public List<GameListDTO> findAll(){
         return gameListRepository.findAll().stream().map(GameListDTO::new).toList();
     }
