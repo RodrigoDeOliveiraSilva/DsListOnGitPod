242e9a58f340a83142632d97cab3f1f405026d27
RodgoDeOliveiraSilva <rodrigodeoliveirasilva20@gmail.com>
2023-05-11 22:55:55 -0300

    Create Game Entity Service By Id.

--- listgame/src/main/java/com/rodi/listgame/services/GameService.java
+++ listgame/src/main/java/com/rodi/listgame/services/GameService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.rodi.listgame.dto.GameDTO;
 import com.rodi.listgame.dto.GameMinDTO;
 import com.rodi.listgame.repositories.GameRepository;
 
@@ -16,8 +17,11 @@ public class GameService {
 
 
     public List<GameMinDTO> findAll(){
-        
         //gameRepository.findAll().stream().map( x -> new GameMinDTO(x)).toList(); --> Another way to do it using lambda function without simplification
         return gameRepository.findAll().stream().map(GameMinDTO::new).toList(); //transforming into a stream, creating an instance of GameMinDTO, transforming into a list
+    }
+
+    public GameDTO findById(Long id){
+        return new GameDTO(gameRepository.findById(id).get());
     }
 }
