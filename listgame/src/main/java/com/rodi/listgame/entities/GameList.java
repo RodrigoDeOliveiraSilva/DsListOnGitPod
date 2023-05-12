6d75b810c5b9f91acf1bdcb4997cb0489bad8788
RodgoDeOliveiraSilva <rodrigodeoliveirasilva20@gmail.com>
2023-05-11 22:20:46 -0300

    JPA mapping Hibernate Entity GameList.

--- listgame/src/main/java/com/rodi/listgame/entities/GameList.java
+++ listgame/src/main/java/com/rodi/listgame/entities/GameList.java
@@ -2,10 +2,20 @@ package com.rodi.listgame.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+@Entity
+@Table(name = "tb_gameList")
 public class GameList  implements Serializable{
     private static final long serialVersionUID = 1L;
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
     private String name;
 
     public GameList() {
