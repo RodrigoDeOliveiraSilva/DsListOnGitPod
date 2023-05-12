a0e25d246868bd7e48da2a8661fc6a76ba9d24d3
RodgoDeOliveiraSilva <rodrigodeoliveirasilva20@gmail.com>
2023-05-11 22:09:11 -0300

    Create entity Belonging 2/2.

--- listgame/src/main/java/com/rodi/listgame/entities/Belonging.java
+++ listgame/src/main/java/com/rodi/listgame/entities/Belonging.java
@@ -1,7 +1,76 @@
 package com.rodi.listgame.entities;
 
-public class Belonging {
+import java.io.Serializable;
+
+import jakarta.persistence.EmbeddedId;
+
+public class Belonging implements Serializable{
+    private static final long serialVersionUID = 1L;
     
-    private BelongingPK id;
+    @EmbeddedId
+    private BelongingPK id = new BelongingPK();
+
     private Integer position;
+
+    public Belonging() {
+    }
+
+    public Belonging(Game game, GameList list, Integer position) {
+        id.setGame(game);
+        id.setList(list);
+        this.position = position;
+    }
+
+    public Game getGame(){
+        return id.getGame();
+    }
+
+    public void setGame(Game game){
+        id.setGame(game);
+    }
+
+    public GameList getGameList(){
+        return id.getList();
+    }
+
+    public void setGameList(GameList list){
+        id.setList(list);
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Belonging other = (Belonging) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    
+
+    
 }
