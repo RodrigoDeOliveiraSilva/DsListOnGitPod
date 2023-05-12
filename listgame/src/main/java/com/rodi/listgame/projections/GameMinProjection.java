b97fe2d4d4910cf989d3d3d6b522c5dfcbe4a5e3
RodgoDeOliveiraSilva <rodrigodeoliveirasilva20@gmail.com>
2023-05-11 23:46:44 -0300

    Create Query Projection JPA of Entity Game.

---
+++ listgame/src/main/java/com/rodi/listgame/projections/GameMinProjection.java
@@ -0,0 +1,12 @@
+package com.rodi.listgame.projections;
+
+public interface GameMinProjection {
+    
+    Long getId();
+    String getTitle();
+    Integer getYear();
+    String getImgUrl();
+    String GetShortDescription();
+    Integer getPosition();
+
+}
