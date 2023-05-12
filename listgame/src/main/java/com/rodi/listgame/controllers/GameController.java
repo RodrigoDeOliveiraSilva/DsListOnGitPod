c19696d5893f7e7dcbd3a1b5d5a249700efcb7a7
RodgoDeOliveiraSilva <rodrigodeoliveirasilva20@gmail.com>
2023-05-11 23:03:30 -0300

    Create Get Mapping Controller of entity Game By Id.

--- listgame/src/main/java/com/rodi/listgame/controllers/GameController.java
+++ listgame/src/main/java/com/rodi/listgame/controllers/GameController.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rodi.listgame.dto.GameDTO;
 import com.rodi.listgame.dto.GameMinDTO;
 import com.rodi.listgame.services.GameService;
+
+import jakarta.websocket.server.PathParam;
 
 @RestController
 @RequestMapping(value = "/games")
@@ -20,6 +24,11 @@ public class GameController {
     @GetMapping
     public List<GameMinDTO> findAll(){
         return gameService.findAll();
+    }
+
+    @GetMapping(value = "/games/{id}")
+    public GameDTO findById(@PathVariable Long id){
+        return gameService.findById(id);
     }
     
 }
--- listgame/src/main/java/com/rodi/listgame/services/GameService.java
+++ listgame/src/main/java/com/rodi/listgame/services/GameService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.rodi.listgame.dto.GameDTO;
 import com.rodi.listgame.dto.GameMinDTO;
@@ -15,12 +16,13 @@ public class GameService {
     @Autowired
     GameRepository gameRepository;
 
-
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         //gameRepository.findAll().stream().map( x -> new GameMinDTO(x)).toList(); --> Another way to do it using lambda function without simplification
         return gameRepository.findAll().stream().map(GameMinDTO::new).toList(); //transforming into a stream, creating an instance of GameMinDTO, transforming into a list
     }
 
+    @Transactional(readOnly = true)
     public GameDTO findById(Long id){
         return new GameDTO(gameRepository.findById(id).get());
     }
