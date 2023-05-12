bf66e266312ec14dc8cfd8890e5d13989aa1d5a2
RodgoDeOliveiraSilva <rodrigodeoliveirasilva20@gmail.com>
2023-05-11 22:27:17 -0300

    Seed Entity GameList.

--- listgame/src/main/java/com/rodi/listgame/entities/GameList.java
+++ listgame/src/main/java/com/rodi/listgame/entities/GameList.java
@@ -8,14 +8,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 @Entity
-@Table(name = "tb_gameList")
+@Table(name = "tb_game_list")
 public class GameList  implements Serializable{
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     private String name;
 
     public GameList() {
