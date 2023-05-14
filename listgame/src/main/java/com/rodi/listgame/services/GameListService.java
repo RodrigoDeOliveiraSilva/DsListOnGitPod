eca5867c6a3c004ba05e20959d2e7cd01ba1bc5c
RodgoDeOliveiraSilva <rodrigodeoliveirasilva20@gmail.com>
2023-05-14 12:05:01 -0300

    Create Service Move - listGame.

--- listgame/src/main/java/com/rodi/listgame/services/GameListService.java
+++ listgame/src/main/java/com/rodi/listgame/services/GameListService.java
@@ -7,17 +7,29 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.rodi.listgame.dto.GameListDTO;
+import com.rodi.listgame.dto.GameMinDTO;
+import com.rodi.listgame.projections.GameMinProjection;
 import com.rodi.listgame.repositories.GameListRepository;
+import com.rodi.listgame.repositories.GameRepository;
 
 @Service
 public class GameListService {
 
     @Autowired
-    GameListRepository gameListRepository;
+    private GameListRepository gameListRepository;
+
+    @Autowired
+    private GameRepository gameRepository;
 
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll(){
         return gameListRepository.findAll().stream().map(GameListDTO::new).toList();
+    }
+
+    public void move(Long listGameId, int sourceIndex, int destinatIndex){
+        List<GameMinProjection> list = gameRepository.searchByList(listGameId);
+        GameMinProjection objGame = list.remove(sourceIndex); // Remove and save to a variable
+        list.add(destinatIndex, objGame); // Adding the object in the variable in the new position of the list
     }
     
 }
--- listgame/src/main/java/com/rodi/listgame/services/GameService.java
+++ listgame/src/main/java/com/rodi/listgame/services/GameService.java
@@ -14,7 +14,7 @@ import com.rodi.listgame.repositories.GameRepository;
 public class GameService {
     
     @Autowired
-    GameRepository gameRepository;
+    private GameRepository gameRepository;
 
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
